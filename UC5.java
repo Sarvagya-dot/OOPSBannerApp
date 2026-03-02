41f4c0c7d80e8a5ea9d3a25d34cdd763e6bdffca
Sarvagya-dot <sg2709@srmist.edu.in>
2026-03-02 09:25:25 +0530

    add code

---
+++ UC5.java
@@ -0,0 +1,22 @@
+public class OOPSBannerApp {
+
+    public static void main(String[] args) {
+
+        // Define and populate the String array inline using String.join()
+        String[] lines = {
+
+            String.join(" ", " *****  ", "  *****  ", " *****  ", "  *****  "),
+            String.join(" ", "*     * ", " *     * ", "*     * ", " *      "),
+            String.join(" ", "*     * ", " *     * ", "*     * ", " *      "),
+            String.join(" ", "*     * ", " *     * ", " *****  ", "  ***** "),
+            String.join(" ", "*     * ", " *     * ", " *      ", "       *"),
+            String.join(" ", "*     * ", " *     * ", " *      ", " *     *"),
+            String.join(" ", " *****  ", "  *****  ", " *      ", "  ***** ")
+        };
+
+        // Use for-each loop to print each line
+        for (String line : lines) {
+            System.out.println(line);
+        }
+    }
+}
