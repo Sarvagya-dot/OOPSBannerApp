4f1ee4de072775240cd37d572f187b397162849d
Sarvagya-dot <sg2709@srmist.edu.in>
2026-03-02 09:06:40 +0530

    UC3

---
+++ UC3.java
@@ -0,0 +1,28 @@
+public class OOPSBannerAppUC3 {
+
+    public static void main(String[] args) {
+
+        // Printing OOPS Banner using String.join()
+
+        System.out.println(String.join(" ", 
+                "  ***  ", "  *****  ", " *****  "));
+
+        System.out.println(String.join(" ", 
+                " **  ** ", " **   ** ", " **   ** "));
+
+        System.out.println(String.join(" ", 
+                "**    **", "**    **", "**    **"));
+
+        System.out.println(String.join(" ", 
+                "**    **", "**    **", " *****  "));
+
+        System.out.println(String.join(" ", 
+                "**    **", "**    **", "    **   "));
+
+        System.out.println(String.join(" ", 
+                " **  ** ", " **   ** ", " **  **  "));
+
+        System.out.println(String.join(" ", 
+                "  ***  ", "  *****  ", " *****  "));
+    }
+}
